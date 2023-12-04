095f420c00bdc617d5a15594745849e050e19b25
Adam Höllerl <a.hoellerl@students.htl-leonding.ac.at>
2023-12-04 10:29:23 +0100

    star 1 day 4 finished

--- Day04/star1.ts
+++ Day04/star1.ts
@@ -3,5 +3,37 @@ import path from 'path';
 
 const data: string = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
 const lines: string[] = data.split(/\r?\n/);
+let points = 0;
 
-console.log("Answer: ");
+for (const line of lines) {
+    const nameAndInput = line.split(": ");
+    const input = nameAndInput[1].split(" | ");
+    const winningNums = input[0].split(" ");
+    const myNums = input[1].split(" ");
+    winningNums.forEach((num, index) => {
+        if (num === "") {
+            winningNums.splice(index, 1);
+        }
+    });
+    myNums.forEach((num, index) => {
+        if (num === "") {
+            myNums.splice(index, 1);
+        }
+    });
+    let currentPoints = 0;
+    for (const myNum of myNums) {
+        for (const winningNum of winningNums) {
+            if (myNum === winningNum) {
+                currentPoints = currentPoints === 0? 1 : currentPoints * 2;
+            }
+
+        }
+
+    }
+    points += currentPoints;
+
+}
+
+
+console.log("Answer:   " + points);
+console.log("Expected: 13");
